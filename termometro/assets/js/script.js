65a4b1a2473fc6103438fccf89e2e65a1e3db226
Gabriel Jurado <81241432+GabrielJurado117@users.noreply.github.com>
2023-11-24 14:56:40 -0300

    Add files via upload

---
+++ termometro/assets/js/script.js
@@ -0,0 +1,52 @@
+const units = {
+	Celcius: "°C",
+	Fahrenheit: "°F"
+};
+
+const config = {
+	minTemp: 2,
+	maxTemp: 35,
+	unit: "Celcius"
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Change min and max temperature values
+
+    const tempValueInputs = document.querySelectorAll("input[type='text']");
+
+    tempValueInputs.forEach((input) => {
+        input.addEventListener("change", (event) => {
+            const newValue = event.target.value;
+            
+            if(isNaN(newValue)) {
+                return input.value = config[input.id];
+            } else {
+                config[input.id] = input.value;
+                range[input.id.slice(0, 3)] = config[input.id]; // Update range
+                return setTemperature(); // Update temperature
+            }
+        });
+    });
+
+    // Change temperature
+    const temperature = document.getElementById("temperature");
+
+    function setTemperature() {
+
+        // Pido los datos a la ruta /data/update
+        fetch("/data/update")
+        .then(response => response.json())
+        .then(data => {
+            // Guardo el valor de temperatura en la variable temp
+            const unconvertedTemp = ((data.lm35_temp * 1.1) / 65536) / 0.01; 
+            const temp = Math.round(unconvertedTemp * 10) / 10
+            
+            // Asigno el valor de temperatura al termometro
+            temperature.style.height = (temp - config.minTemp) / (config.maxTemp - config.minTemp) * 100 + "%";
+            temperature.dataset.value = temp + units[config.unit];
+        });
+        
+    }
+    // Se actualiza la temperatura cada 200 ms
+    setInterval(setTemperature, 200);
+});
